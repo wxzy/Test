89f61ce92f720b05653853759fa074a703b66a69
Jeffrey Mathew <jmath81143@my.sunywcc.edu>
2017-05-04 14:23:32 -0400

    Version 2

--- src/org/usfirst/frc/team2344/robot/Robot.java
+++ src/org/usfirst/frc/team2344/robot/Robot.java
@@ -1,63 +1,60 @@
-package org.usfirst.frc.team2344.robot;
-
-import com.ctre.CANTalon;
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.RobotDrive;
-/*
- *  FIRST® Team 2344
- * 		   2017 OffSeason - Malone
+/* 
+ * FIRST Team 2344 - Saunders Droid Factory
+ * FRC Season - SteamWorks
+ * Developer - Jeffrey A. Mathew
  */
 
+package org.usfirst.frc.team2344.robot;
+import com.ctre.CANTalon;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
 public class Robot extends IterativeRobot {
-	/* 
-	 *  Motors
-	 *  	On Malone, we have eight (8) motors, of which six (6) controls the drive
-	 * 		station and the other two (2) control the lifting procedure. They will be
-	 *      controlled via TalonSRX in a CAN Bus.
+	
+	/*
+	 * Drive 
+	 *  - There are in total six (6) motors, in which two (2) will be defined the 
+	 *  Master, and the rest as slaves.
+	 *  
+	 *  - There is also one solenoid to control the "shifting" or changing from
+	 *  low gear mode to high gear mode. 
+	 *  
 	 */
-	   //Right Motors
-		CANTalon DriveRight = new CANTalon(1);
-			//Slave Motors
-			CANTalon RightFront = new CANTalon(2);
-			CANTalon RightRear = new CANTalon(3);
+		//Right Side Motors 
+			// Master
+				CANTalon DriveRight = new CANTalon(4);
+			// Slave
+				CANTalon RightFront = new CANTalon(5);
+				CANTalon RightRear  = new CANTalon(6);
+				
+		//Left Side Motors
+			// Master
+				CANTalon DriveLeft  = new CANTalon(1);
+			// Slave
+				CANTalon LeftFront  = new CANTalon(2);
+				CANTalon LeftRear   = new CANTalon(3);
+	 
+			   RobotDrive Test = new RobotDrive(DriveLeft, DriveRight);
+		// JoySticks
+			Joystick Joy = new Joystick(0);
 			
-		//Left Motors	
-		CANTalon DriveLeft = new CANTalon(4);
-			//Slave Motors
-			CANTalon LeftFront = new CANTalon(5);
-			CANTalon LeftRear = new CANTalon(6);
+			
+		@Override
+		public void robotInit(){
+			RightFront.changeControlMode(CANTalon.TalonControlMode.Follower);
+				RightFront.set(DriveRight.getDeviceID());
+			RightRear.changeControlMode(CANTalon.TalonControlMode.Follower);
+				RightRear.set(DriveRight.getDeviceID());
+			LeftFront.changeControlMode(CANTalon.TalonControlMode.Follower);
+				LeftFront.set(DriveLeft.getDeviceID());
+			LeftRear.changeControlMode(CANTalon.TalonControlMode.Follower);
+				LeftRear.set(DriveLeft.getDeviceID());
+			}
 		
-		// Lifting Motors
-		CANTalon LiftLeft = new CANTalon(7);
-		CANTalon LiftRight = new CANTalon(8);
-		
-		//Robot Drive
-		RobotDrive Malone;
-		
-		public void robotInit() {
-			//Right Motors
-			// - Set DriveRight as Master
-			// - Set RightFront & RightRear as the Slaves
-			DriveRight.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
-				RightFront.changeControlMode(CANTalon.TalonControlMode.Follower);
-					RightFront.set(DriveRight.getDeviceID());
-				RightRear.changeControlMode(CANTalon.TalonControlMode.Follower);
-					RightRear.set(DriveRight.getDeviceID());
-			
-			//Left Motors
-			// - Set DriveLeft as the Master
-			// - Set LeftFront & LeftRear as the Slaves
-			DriveLeft.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
-				LeftFront.changeControlMode(CANTalon.TalonControlMode.Follower);
-					LeftFront.set(DriveLeft.getDeviceID());
-				LeftRear.changeControlMode(CANTalon.TalonControlMode.Follower);
-					LeftRear.set(DriveLeft.getDeviceID());
-			//Lift Motors
-			LiftLeft.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
-			LiftRight.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
-			
-			//RobotDrive
-			Malone = new RobotDrive(DriveRight,DriveLeft);
-			
+		@Override
+		public void teleopPeriodic() {
+			double left = Joy.getRawAxis(1); // logitech gampad left X, positive is forward
+	    	double right = Joy.getRawAxis(3); //logitech gampad right X, positive means turn right
+	    	Test.tankDrive(left, right);
 		}
 }
